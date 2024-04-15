e993a0b65a4921c7e03e839ae34da634291694c2
ellen-ls <ellen.l.s1996@gmail.com>
2024-04-15 16:34:17 -0300

    preview and filter

--- ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,7 @@ import Container from '@/components/ui/container'
 import Filter from './components/filter'
 import NoResults from '@/components/ui/no-results'
 import ProductCard from '@/components/ui/product-card'
+import Mobilefilters from './components/mobile-filter'
 
 interface CategoryPageProps{
     params:{
@@ -37,11 +38,15 @@ interface CategoryPageProps{
     <div className='bg-white'>
       <Container>
         <Billboard 
-        data={category.billboard}
+        data={category?.billboard}
         />
         <div className='px-4 sm:px-6 lg:px-8 pb-24'>
           <div className='lg:grid lg:grid-cols-5 lg:gap-x-8'>
-            {/* Add Mobile Filters */}
+           <Mobilefilters
+           sizes={sizes}
+           colors={colors}
+           />
+        
             <div className='hidden lg:block'>
               <Filter
               valueKey="sizeId"
--- ecommerce-store/types.ts
+++ ecommerce-store/types.ts
@@ -1,40 +1,37 @@
-export interface Billboard{
-    label: string;
-    id: string;
-    imageUrl: string;
+export interface Billboard {
+    id: string
+    label:string
+    imageUrl:string
 }
 
-export interface Category{
-    id: string;
-    name: string;
-    billboard: Billboard;
+export interface Category {
+    id: string
+    name:string
+    billboard: Billboard
 }
 
-export interface Product{
-    id: string;
-    category: Category;
-    name: string;
-    price: string;
-    isFeatured: boolean;
-    size: Size;
-    Color: Color;
-    images: Image[];
-
+export interface Product {
+    id:string
+    category: Category
+    name:string
+    price:string
+    isFeatured:boolean
+    size:Size
+    Color:Color
+    images:Image[]
 }
 
-export interface Image{
-    id: string;
-    url: string;
+export interface Image {
+    id:string
+    url:string
 }
-
-export interface Size{
-    id: string;
-    name: string;
-    value: string
+export interface Size {
+    id:string
+    name:string
+    value:string
 }
-
-export interface Color{
-    id: string;
-    name: string;
-    value: string
+export interface Color {
+    id:string
+    name:string
+    value:string
 }
