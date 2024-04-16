0830c146760dfa47d738d415994ec82cefead242
ellen-ls <ellen.l.s1996@gmail.com>
2024-04-15 22:22:38 -0300

    building cart

--- ecommerce-store/components/ui/product-card.tsx
+++ ecommerce-store/components/ui/product-card.tsx
@@ -8,6 +8,7 @@ import Currency from "./currency"
 import { useRouter } from "next/navigation"
 import { MouseEventHandler } from "react"
 import usePreviewModal from "@/hooks/use-preview-modal"
+import useCart from "@/hooks/use-cart"
 
 interface ProductCard{
     data: Product
@@ -17,6 +18,7 @@ const ProductCard: React.FC<ProductCard> =({
     data
 })=>{
     
+    const cart = useCart()
     const previewModal = usePreviewModal()
     const router = useRouter()
 
@@ -29,6 +31,14 @@ const ProductCard: React.FC<ProductCard> =({
         previewModal.onOpen(data)
 
     }
+
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) =>{
+        event.stopPropagation()
+        cart.addItem(data)
+
+    }
+
+
     return(
         <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
            {/* Images and Actions*/}
@@ -46,7 +56,7 @@ const ProductCard: React.FC<ProductCard> =({
                     icon={<Expand size={20} className="text-gray-600"/>}
                     />
                     <IconButton
-                    onClick={()=>{}}
+                    onClick={onAddToCart}
                     icon={<ShoppingCart size={20} className="text-gray-600"/>}
                     />
                 </div>
